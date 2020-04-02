e0a12576aad3ca607fd0f584245dbdd4f9cf3e6a
Jonathan Barnoud <jonathan@barnoud.net>
2020-04-02 17:50:44 +0100

    Retrieve IP address

--- create_compute_instance.py
+++ create_compute_instance.py
@@ -1,4 +1,6 @@
 import oci
+import time
+import datetime
 
 OCID = {
     'user': 'ocid1.user.oc1..aaaaaaaahtozgcevfxlu462v5trk6sldbfz7hckf2cfz4nm3udjxo6puhtla',
@@ -8,6 +10,7 @@ OCID = {
     'availability_domain': 'DpyF:EU-FRANKFURT-1-AD-3',
     'compute_shape': 'VM.GPU2.1',
 }
+LIFECYCLE_STATE_PROVISIONING = oci.core.models.Instance.LIFECYCLE_STATE_PROVISIONING
 
 
 class NotEnoughRessources(Exception):
@@ -20,6 +23,7 @@ def launch_compute_instance():
     compartment_id = OCID['compartment']
 
     compute_client = oci.core.ComputeClient(config)
+    compute_composite = oci.core.ComputeClientCompositeOperations(compute_client)
     instance_details = oci.core.models.LaunchInstanceDetails(
         availability_domain=OCID['availability_domain'],
         compartment_id=compartment_id,
@@ -28,7 +32,11 @@ def launch_compute_instance():
         subnet_id=OCID['subnet'],
     )
     try:
-        response = compute_client.launch_instance(instance_details)
+        response = compute_composite.launch_instance_and_wait_for_state(
+                instance_details,
+                wait_for_states=[LIFECYCLE_STATE_PROVISIONING],
+                waiter_kwargs={'max_interval_seconds': 1},
+            )
     except oci.exceptions.ServiceError as error:
         if error.code == 'LimitExceeded':
             raise NotEnoughRessources
@@ -37,10 +45,40 @@ def launch_compute_instance():
     return instance_id
 
 
+def _get_public_ip(compute_client, virtual_network_client, instance):
+    vnic_attached = compute_client.list_vnic_attachments(
+        compartment_id=instance.compartment_id,
+        instance_id=instance.id).data
+    if vnic_attached:
+        vnic_id = vnic_attached[0].vnic_id
+        vnic = virtual_network_client.get_vnic(vnic_id).data
+        return vnic.public_ip
+    return None
+
+
+def check_instance(instance_id):
+    config = oci.config.from_file()
+    config['user'] = OCID['user']
+
+    compute_client = oci.core.ComputeClient(config)
+    virtual_network_client = oci.core.VirtualNetworkClient(config)
+    instance = compute_client.get_instance(instance_id).data
+    instance_status = instance.lifecycle_state
+    public_ip = _get_public_ip(compute_client, virtual_network_client, instance)
+    return (instance_status, public_ip)
+
+
 if __name__ == '__main__':
     try:
-        launch_compute_instance()
+        instance_id = launch_compute_instance()
     except NotEnoughRessources:
         print('Sorry, could not launch a new instance. Try again latter.')
     except:
         print('Arrgh!')
+        raise
+    else:
+        print(type(instance_id), instance_id)
+        while True:
+            time.sleep(5)
+            print(datetime.datetime.now())
+            print(check_instance(instance_id))
