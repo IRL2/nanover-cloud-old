5a9c03fdd9bcbca2dc8b8e888648efb82b85c7f2
Jonathan Barnoud <jonathan@barnoud.net>
2020-04-02 14:32:36 +0100

    Make a function

--- create_compute_instance.py
+++ create_compute_instance.py
@@ -9,22 +9,38 @@ OCID = {
     'compute_shape': 'VM.GPU2.1',
 }
 
-config = oci.config.from_file()
-config['user'] = OCID['user']
-compartment_id = OCID['compartment']
 
-compute_client = oci.core.ComputeClient(config)
-instance_details = oci.core.models.LaunchInstanceDetails(
-    availability_domain=OCID['availability_domain'],
-    compartment_id=compartment_id,
-    image_id=OCID['image'],
-    shape=OCID['compute_shape'],
-    subnet_id=OCID['subnet'],
-)
-try:
-    response = compute_client.launch_instance(instance_details)
-except oci.exceptions.ServiceError:
-    print('Sorry, could not launch a new instance. Try again latter.')
-else:
+class NotEnoughRessources(Exception):
+    pass
+
+
+def launch_compute_instance():
+    config = oci.config.from_file()
+    config['user'] = OCID['user']
+    compartment_id = OCID['compartment']
+
+    compute_client = oci.core.ComputeClient(config)
+    instance_details = oci.core.models.LaunchInstanceDetails(
+        availability_domain=OCID['availability_domain'],
+        compartment_id=compartment_id,
+        image_id=OCID['image'],
+        shape=OCID['compute_shape'],
+        subnet_id=OCID['subnet'],
+    )
+    try:
+        response = compute_client.launch_instance(instance_details)
+    except oci.exceptions.ServiceError as error:
+        if error.code == 'LimitExceeded':
+            raise NotEnoughRessources
+        raise error
     instance_id = response.data.id
-    print(instance_id)
+    return instance_id
+
+
+if __name__ == '__main__':
+    try:
+        launch_compute_instance()
+    except NotEnoughRessources:
+        print('Sorry, could not launch a new instance. Try again latter.')
+    except:
+        print('Arrgh!')
