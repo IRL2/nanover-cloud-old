9bb2cf5bfb5bbd6df750f486ca45f846dc75256e
Jonathan Barnoud <jonathan@barnoud.net>
2020-04-03 15:44:17 +0100

    Basic web server works locally

--- naas_server/app/routes.py
+++ naas_server/app/routes.py
@@ -1,7 +1,15 @@
 import datetime
 import uuid
+import oci
+from . import libinstance
 from flask import render_template, url_for, redirect
 from app import app
+
+STATES_AVAILABLE = (
+    oci.core.models.Instance.LIFECYCLE_STATE_PROVISIONING,
+    oci.core.models.Instance.LIFECYCLE_STATE_RUNNING,
+)
+
 
 @app.route('/')
 @app.route('/index')
@@ -11,14 +19,31 @@ def index():
 
 @app.route('/launch')
 def launch():
-    job_id = uuid.uuid4()
+    try:
+        job_id = libinstance.launch_compute_instance()
+    except libinstance.NotEnoughRessources:
+        return 'No resource available. Try again later.'
+    except Exception as err:
+        print(err)
+        return 'Something unexpected happened.'
     return redirect(url_for('status', job_id=job_id))
 
 
 @app.route('/status/<job_id>')
 def status(job_id):
+    available = True
+    ip = ''
+    narupa_status = False
+    try:
+        oci_state, ip, narupa_status = libinstance.check_instance(job_id)
+    except Exception as err:
+        print(err)
+        available = False
+    if oci_state not in STATES_AVAILABLE:
+        available = False
     variables = {
-        'now': str(datetime.datetime.now()),
-        'job_id': job_id,
+        'ip': ip,
+        'narupa_status': narupa_status,
+        'available': available,
     }
     return render_template('status.html', **variables)
