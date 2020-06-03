bc2925ea904f6b2ee7067dd34878e59b00cece0c
Jonathan Barnoud <jonathan@barnoud.net>
2020-06-03 16:23:40 +0200

    Add json API to the server

--- naas_server/app/routes.py
+++ naas_server/app/routes.py
@@ -3,7 +3,8 @@ import datetime
 import uuid
 import oci
 from . import libinstance
-from flask import render_template, url_for, redirect, request
+from flask import render_template, url_for, redirect, request, jsonify
+from werkzeug.exceptions import BadRequest, NotFound
 from app import app
 
 STATES_AVAILABLE = (
@@ -12,6 +13,15 @@ STATES_AVAILABLE = (
 )
 DEFAULT_FILENAME = 'helen.xml'
 MANIFEST = 'https://gitlab.com/intangiblerealities/narupacloud/narupa-cloud-simulation-inputs/-/raw/master/manifest.txt'
+REGIONS = {
+    'Frankfurt': {'url': 'https://staging.narupa.xyz', 'string': 'eu-frankfurt-1'},
+    'London': {'url': 'http://152.67.129.75', 'string': 'uk-london-1'},
+    'Ashburn': {'url': 'http://129.213.120.237', 'string': 'iad'},
+}
+REGION_STRINGS = {
+    description['string']: region
+    for region, description in REGIONS.items()
+}
 
 
 def available_inputs():
@@ -102,3 +112,103 @@ def status(job_id):
 def terminate(job_id):
     libinstance.terminate_instance(job_id)
     return redirect(url_for('status', job_id=job_id))
+
+
+@app.route('/local/v1/instance/<job_id>', methods=['GET'])
+def local_status(job_id):
+    available = True
+    ip = ''
+    narupa_status = False
+    oci_state = None
+    try:
+        oci_state, ip, narupa_status = libinstance.check_instance(job_id)
+    except Exception as err:
+        print(err)
+        available = False
+    if available and oci_state not in STATES_AVAILABLE:
+        available = False
+    variables = {
+        'ip': ip,
+        'narupa_status': narupa_status,
+        'available': available,
+        'oci_state': oci_state,
+    }
+    return jsonify(variables)
+
+
+@app.route('/local/v1/instance/<job_id>', methods=['DELETE'])
+def local_terminate(job_id):
+    libinstance.terminate_instance(job_id)
+    return jsonify('')
+
+
+@app.route('/local/v1/instance', methods=['POST'])
+def local_launch():
+    if not request.is_json:
+        raise BadRequest
+    try:
+        filename=request.json['simulation']
+    except:
+        raise BadRequest
+
+    region = request.json.get('region', 'Frankfurt')
+    extra_meta = {
+        'branch': request.form.get('branch', 'master'),
+        'runner': request.form.get('runner', 'ase'),
+    }
+
+    try:
+        job_id = libinstance.launch_compute_instance(
+                filename, region=region, image='git', extra_meta=extra_meta)
+    except libinstance.NotEnoughRessources:
+        return jsonify({'status': 'not enough ressources'})
+    except:
+        return jsonify({'status': 'failed'})
+    
+    return jsonify({'status': 'success', 'jobid': job_id})
+
+
+def region_from_job_id(job_id):
+    region_string = job_id.split('.')[3]
+    return REGION_STRINGS[region_string]
+
+
+def get_current_region():
+    ocid = requests.get('http://169.254.169.254/opc/v1/instance/id').text
+    return region_from_job_id(ocid)
+
+
+@app.route('/api/v1/instance/<job_id>', methods=['GET'])
+def api_status(job_id):
+    region = region_from_job_id(job_id)
+    if region == get_current_region():
+        return local_status(job_id)
+    if region not in REGIONS:
+        raise NotFound
+    return requests.get(f"{REGIONS[region]['url']}/local/v1/instance/{job_id}")
+
+
+@app.route('/api/v1/instance/<job_id>', methods=['DELETE'])
+def api_terminate(job_id):
+    region = region_from_job_id(job_id)
+    if region == get_current_region():
+        return local_terminate(job_id)
+    if region not in REGIONS:
+        raise NotFound
+    return requests.delete(f"{REGIONS[region]['url']}/local/v1/instance/{job_id}")
+
+
+@app.route('/api/v1/instance', methods=['POST'])
+def api_launch():
+    if not request.is_json:
+        raise BadRequest
+    region = request.json.get('region', 'Frankfurt')
+    if region == get_current_region():
+        return local_launch()
+    if region not in REGIONS:
+        raise NotFound
+    return requests.post(
+        f"{REGIONS[region]['url']}/local/v1/instance",
+        data=request.json,
+        headers={"Content-Type": "application/json"},
+    )
