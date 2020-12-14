03963b3cdfe07a32660fe01f00416f9bad11f903
Jonathan Barnoud <jonathan@barnoud.net>
2020-12-14 15:14:03 +0000

    Replace beta domain to app

--- naas_server/init-letsencrypt.sh
+++ naas_server/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(beta.narupa.xyz)
+domains=(app.narupa.xyz)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="hello@narupa.xyz" # Adding a valid address is strongly recommended
