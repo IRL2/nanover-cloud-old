158c008f822efa95c6e645620eb705fec2887ae0
Jonathan Barnoud <jonathan@barnoud.net>
2021-06-14 16:42:11 +0100

    Build the VM image for simulation based on ubuntu
    Instead of using the nvidia image that requires a manual operation to
    accept the license, build the VM image based on the ubuntu base image
    and install cuda.

--- naas_simulation/create_simulation_image.sh
+++ naas_simulation/create_simulation_image.sh
@@ -14,8 +14,8 @@ gcloud compute --project=narupa-web-ui instances create $image_name \
 	--maintenance-policy=TERMINATE \
 	--scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
 	--tags=$image_name \
-	--image=nvidia-gpu-cloud-image-20200629 \
-	--image-project=nvidia-ngc-public \
+	--image-family=ubuntu-minimal-2004-lts \
+	--image-project=ubuntu-os-cloud \
 	--boot-disk-size=50GB \
 	--boot-disk-type=pd-standard \
 	--boot-disk-device-name=$image_name \
@@ -29,8 +29,8 @@ echo "Waiting for instance to come up"
 sleep 60
 
 # We should be able to run this automatically, but for now, prompt for manual
-echo "Please run this command and follow the instructions: gcloud compute ssh $image_name"
-read -p "Once finished, press enter to continue"
+#echo "Please run this command and follow the instructions: gcloud compute ssh $image_name"
+#read -p "Once finished, press enter to continue"
 
 # Update packages
 gcloud compute scp 'update_simulation_image.sh' "$image_name:"
--- naas_simulation/update_simulation_image.sh
+++ naas_simulation/update_simulation_image.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
+set -e
 cd /
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install git software-properties-common wget
+
+DISTRO='ubuntu2004'
+VERSION='1804'
+ARCHITECTURE='x84_64'
+#sudo dpkg -i cuda-repo-${DISTRO}_${VERSION}_${ARCHITECTURE}.deb
+#sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/${DISTRO}/${ARCHITECTURE}/ /"
+#sudo apt-key add /var/cuda-repo-${DISTRO}-${VERSION}/7fa2af80.pub
+#sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/${DISTRO}/${ARCHITECTURE}/7fa2af80.pub
+#wget https://developer.download.nvidia.com/compute/cuda/repos/${DISTRO}/${ARCHITECTURE}/cuda-${DISTRO}.pin
+#sudo mv cuda-${DISTRO}.pin /etc/apt/preferences.d/cuda-repository-pin-600
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install cuda
 
 MINICONDA_PATH="/miniconda"
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
@@ -8,7 +31,7 @@ PATH="${MINICONDA_PATH}/bin:$PATH"
 conda init bash
 source $HOME/.bashrc
 conda update -y -n base -c defaults conda
-conda install -y -c conda-forge python=3.7
+conda install -y -c conda-forge python=3.8
 conda install -y -c conda-forge openmm MDAnalysis MDAnalysisTests ase mpi4py
 pip install --ignore-installed grpcio
 PYTHON=$MINICONDA_PATH/bin/python
@@ -16,4 +39,4 @@ export PATH=$MINICONDA_PATH/bin:$PATH
 
 git clone https://gitlab.com/intangiblerealities/narupa-protocol.git --branch master narupa-protocol
 cd narupa-protocol
-./compile.sh
+./compile.sh --no-dotnet
